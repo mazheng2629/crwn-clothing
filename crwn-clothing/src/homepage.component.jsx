aee3b670322901d686c1f82aee50f78b3a1de3f8
Zheng Ma <macbook@Zhengs-MBP.broadband>
2020-10-05 14:01:17 +0100

    add homepage

---
+++ crwn-clothing/src/homepage.component.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./homepage.style.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Hats</h1>
+          <span className="subtitle">shop</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Hats</h1>
+          <span className="subtitle">shop</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Hats</h1>
+          <span className="subtitle">shop</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Hats</h1>
+          <span className="subtitle">shop</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
